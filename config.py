9d9960458c5b4b021b969997108af336a72df2e4
marina <sadrinamarina88@gmail.com>
2026-04-22 02:52:20 +0500

    чек

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 bot_token="8231168960:AAFzZEkopNLDn_EbE5-LMK5W4NiVPRv2gEQ"
 
-db_url="postgresql://botuser:strong_password_123@localhost:5432/fitness_bot"
+db_url="postgresql://botuser:strong_password_123@localhost:5432/fitness_bot?sslmode=disable"
