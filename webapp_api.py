6a09354495078c3a7d450a6d06022d8afebc47bc
marina <sadrinamarina88@gmail.com>
2026-04-22 13:03:02 +0500

    start page

--- webapp_api.py
+++ webapp_api.py
@@ -1,10 +1,8 @@
 # webapp_api.py
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 from app.webapp.api import router
-import os
-
-from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(
     title="Fitness Bot Web App",
@@ -18,18 +16,21 @@ app.add_middleware(
     allow_origins=[
         "https://web.telegram.org",
         "https://t.me",
-        "https://telegram.org"
+        "https://telegram.org",
+        # Если ты открываешь в браузере напрямую с другого IP
+        "http://localhost:8000",
+        "http://127.0.0.1:8000"
     ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-    allow_credentials=True,
 )
+
+# Подключаем статические файлы из папки static
+app.mount("/app", StaticFiles(directory="static", html=True), name="static")
 
 app.include_router(router)
 
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("webapp_api:app", host="0.0.0.0", port=8000, reload=True)
-
-
-app.mount("/app", StaticFiles(directory="static", html=True), name="static")
