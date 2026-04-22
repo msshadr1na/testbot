3b8301b2ef69f6a89563bb823ab0a61c480a40b1
marina <sadrinamarina88@gmail.com>
2026-04-22 20:12:16 +0500

    test html

--- webapp_api.py
+++ webapp_api.py
@@ -31,6 +31,11 @@ async def get_orgs_page():
     with open("static/org_orgs.html", encoding="utf-8") as f:
         return HTMLResponse(content=f.read())
 
+@app.get("/test", response_class=HTMLResponse)
+async def test_page():
+    with open("static/test.html", encoding="utf-8") as f:
+        return HTMLResponse(content=f.read())
+
 app.include_router(router)
 
 if __name__ == "__main__":
